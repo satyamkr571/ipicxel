7d2176dec74a22c138b5260a1de85ce66aa7e783
Satyam Kumar <satyam@Satyams-MacBook-Air.local>
2020-08-30 00:05:45 +0530

    ui enhanced

--- client/src/components/layout/landingpage.js
+++ client/src/components/layout/landingpage.js
@@ -1,13 +1,6 @@
 import React from "react";
 import "./landingpage.css";
-import {
-  InputGroup,
-  Row,
-  Col,
-  FormControl,
-  Dropdown,
-  DropdownButton,
-} from "react-bootstrap";
+import { InputGroup, Row, Col, FormControl } from "react-bootstrap";
 
 export default function Landingpage() {
   return (
@@ -20,46 +13,27 @@ export default function Landingpage() {
           </p>
         </div>
         <div className="search-container">
-          <Row>
-            <Col xs={1}></Col>
-            <Col xs={10}>
-              <InputGroup className="formdesigne">
-                <DropdownButton title="All Cateogery" variant="light">
-                  <Dropdown.Item href="#/action-2">
-                    <i className="material-icons">camera_alt</i>
-                    <p className="selectedText">Images</p>
-                  </Dropdown.Item>
-                  <Dropdown.Item href="#/action-3">
-                    <i className="material-icons">music_video</i>
-                    <p className="selectedText">Music</p>
-                  </Dropdown.Item>
-                  <Dropdown.Item href="#/action-3">
-                    <i className="material-icons">video_library</i>
-                    <p className="selectedText">Video</p>
-                  </Dropdown.Item>
-                </DropdownButton>
-
-                <FormControl
-                  placeholder="Sreach..."
-                  aria-label="Search"
-                  style={{
-                    height: "100%",
-                    marginTop: "0px",
-                    fontSize: "2vw",
-                    border: "0px",
-                  }}
-                />
-                <InputGroup.Append>
-                  <button variant="outline-secondary" className="buttondesgine">
-                    <i className="material-icons" style={{ top: "0px" }}>
-                      search
-                    </i>
-                  </button>
-                </InputGroup.Append>
-              </InputGroup>
-            </Col>
-            <Col xs={1}></Col>
-          </Row>
+          <div className="row searchbox">
+            <div className="col-3 searchboxselect">
+              <select class="mdb-select searchboxselecter">
+                <option value="0">Select Your Tag</option>
+                <option value="1">Music</option>
+                <option value="2">Picture</option>
+                <option value="3">Videos</option>
+                <option value="4">Indian Images</option>
+              </select>
+            </div>
+            <div className="col-6 searchboxinput">
+              <form className="searchboxinput">
+                <input
+                  className="form-control searchboxforminput"
+                  type="text"
+                  placeholder="Search"
+                ></input>
+              </form>
+            </div>
+            <div className="col-3 searchboxbutton"></div>
+          </div>
         </div>
       </div>
     </div>
